8827e97077178c465eeda371c20eaf3c93dcfa05
Andrew Courter <exosyphon@yahoo.com>
2022-03-22 00:51:18 -0500

    Update title and description

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,8 @@ function Index() {
   return (
     <div>
       <Head>
-        <title>App</title>
-        <meta name="description" content="An app" />
+        <title>Andrew Courter</title>
+        <meta name="description" content="Andrew Courter's site" />
       </Head>
       <main className="flex flex-col justify-center items-center h-screen">
         <div className="text-5xl">Hello! My name is Andrew Courter</div>

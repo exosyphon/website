6cbb7ccab87f3f797fa5fc8b61900166822412bc
Andrew Courter <exosyphon@yahoo.com>
2022-06-18 08:01:39 -0500

    Add new general help link to Services

--- pages/services.tsx
+++ pages/services.tsx
@@ -18,6 +18,12 @@ const Services = () => {
                             <a className='text-2xl underline hover:italic'>Interview Help</a>
                         </Link>
                     </div>
+                    <div className='flex flex-col items-center justify-center'>
+                        <div className='text-center'>Need general help? Send me a request.</div>
+                        <Link href="https://forms.gle/jDdrYkbswhiUoPJ38">
+                            <a className='text-2xl underline hover:italic'>General Help</a>
+                        </Link>
+                    </div>
                     <div className='flex flex-col items-center justify-center mt-2'>
                         <div className='text-center'>Interested in advertising content on my blog or Youtube?</div>
                         <div className='text-2xl'>DM me on <Link href='https://twitter.com/Exosyphon'><a className='underline hover:italic'>Twitter</a></Link></div>
